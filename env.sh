2ecec9a0bb35e0f5cf9e7309b27a811ebdfed055
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2021-05-15 17:44:49 -0500

    Fix env.sh script

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # line endings must be \n, not \r\n !
-echo "window._env_ = {" > ./env-config.js
+echo "window._env_ = {" > /var/www/env-config.js
 (printenv | grep REACT_APP) > .env
-awk -F '=' '{ print $1 ": \"" (ENVIRON[$1] ? ENVIRON[$1] : $2) "\"," }' ./.env >> ./env-config.js
+awk -F '=' '{ print $1 ": \"" (ENVIRON[$1] ? ENVIRON[$1] : $2) "\"," }' ./.env >> /var/www/env-config.js
 echo "}" >> /var/www/env-config.js
