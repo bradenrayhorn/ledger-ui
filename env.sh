8d26773dcf1cd6406c325ae245b63a27a2c1c1b8
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2021-05-20 17:11:18 -0500

    Update env script

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # line endings must be \n, not \r\n !
 echo "window._env_ = {" > /var/www/env-config.js
-(printenv | grep REACT_APP) > .env
-awk -F '=' '{ print $1 ": \"" (ENVIRON[$1] ? ENVIRON[$1] : $2) "\"," }' ./.env >> /var/www/env-config.js
+(cat .env | grep REACT_APP) > .react-env
+awk -F '=' '{ print $1 ": \"" (ENVIRON[$1] ? ENVIRON[$1] : $2) "\"," }' ./.react-env >> /var/www/env-config.js
 echo "}" >> /var/www/env-config.js
